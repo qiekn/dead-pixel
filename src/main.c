9f72ea951b3b52b7e37ff7156e0a852678632365
Seb Zanardo <sebzanardo@optusnet.com.au>
2025-02-09 17:10:53 +1100

    Added raylib basic example to src/main.c

---
+++ src/main.c
@@ -0,0 +1,20 @@
+// Basic example (raylib)
+// https://github.com/raysan5/raylib
+#include "raylib.h"
+
+int main(void)
+{
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
