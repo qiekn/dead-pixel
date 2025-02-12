2a3946ef1e7f80a4861144122e7ae4886cd65849
Seb Zanardo <sebzanardo@optusnet.com.au>
2025-02-12 20:01:47 +1100

    Improved build script to only run .exe if built successfully

--- build.sh
+++ build.sh
@@ -2,5 +2,10 @@
 # https://github.com/raysan5/raylib/wiki/Working-on-GNU-Linux
 cc src/main.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o game.exe
 
+# Exit the script if the last command was unsuccessful
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+
 # Run the executable that was created
 ./game.exe
