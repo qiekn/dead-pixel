2bedf470f852ecf55686941d58622021c20089b6
Seb Zanardo <sebzanardo@optusnet.com.au>
2025-03-01 12:03:15 +1100

    Editted default template

--- src/main.c
+++ src/main.c
@@ -1,20 +1,20 @@
-// Basic example (raylib)
-// https://github.com/raysan5/raylib
 #include "raylib.h"
 
-int main(void)
-{
-    InitWindow(800, 450, "raylib [core] example - basic window");
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
+const int FPS = 60;
+const float FIXED_DT = 1.0f / FPS;
 
-    while (!WindowShouldClose())
-    {
+int main(void) {
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "playmakers-jam");
+    SetTargetFPS(FPS);
+
+    while (!WindowShouldClose()) {
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        ClearBackground(BLACK);
+        DrawFPS(0, 0);
         EndDrawing();
     }
 
     CloseWindow();
-
-    return 0;
 }
